bc0f1b98c23eff4e3ce4a6b6d66836c30da3e08f
somename <someemail>
2017-06-08 20:56:33 +0200

    besouro automatic message

--- src/PlanetExplorer.java
+++ src/PlanetExplorer.java
@@ -5,10 +5,10 @@
 
 public class PlanetExplorer {
 	public int x, y;
-	String obstacles;
-	int voziloX = 0;
-	int voziloY=0;
-	int smer=0;
+	public String obstacles;
+	public int voziloX = 0;
+	public int voziloY=0;
+	public int smer=0;
 	public PlanetExplorer(int x, int y, String obstacles){
 	/*	x and y represent the size of the grid.
 	 *  Obstacles is a String formatted as follows: "(obs1_x,obs1_y)(obs2_x,obs2_y)...(obsN_x,obsN_y)" with no white spaces. 
@@ -38,7 +38,7 @@ public class PlanetExplorer {
 		 * Where pos_x and pos_y are the final coordinates, facing is the current direction the explorer is pointing to (N,S,W,E).
 		 * The return string should also contain a list of coordinates of the encountered obstacles. No white spaces.
 		 */
-		int j = 0;
+	
 		switch(command){
 			case "f":
 				return "(0,1,N)";
@@ -102,7 +102,7 @@ public class PlanetExplorer {
 			
 		}
 		
-		if(obstacles ==pozicija)
+		if(obstacles==pozicija)
 			throw new PlanetExplorerException();
 		if (smer==0)
 		return "(" + voziloX + "," + voziloY +"," + "N)";
--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -29,22 +29,28 @@ public class TestPlanetExplorer {
 		String result=explorer.executeCommand("b");
 		assertEquals("(0,2,N)", result);
 	}
-	/*@Test (expected=PlanetExplorerException.class)
+	@Test (expected=PlanetExplorerException.class)
 	public void test_VoziloUCelijiSaPreprekom() throws PlanetExplorerException{
 		PlanetExplorer explorer = new PlanetExplorer(3,3 ,"(0,1)");
 		explorer.executeCommand("f");
-	}*/
+	}
 	@Test 
 	public void test_ffrf()throws PlanetExplorerException{
 		PlanetExplorer explorer = new PlanetExplorer(3,3 ,null);
-		String result=explorer.executeCommand("b");
+		String result=explorer.executeCommand("ffrf");
 		assertEquals("(1,2,E)", result);
 	}
 	@Test 
 	public void test_ff() throws PlanetExplorerException{
 		PlanetExplorer explorer = new PlanetExplorer(3,3 ,null);
-		String result=explorer.executeCommand("b");
+		String result=explorer.executeCommand("ff");
 		assertEquals("(0,2,N)", result);
+	}
+	@Test 
+	public void test_rr() throws PlanetExplorerException{
+		PlanetExplorer explorer = new PlanetExplorer(3,3 ,null);
+		String result=explorer.executeCommand("rr");
+		assertEquals("(0,0,E)", result);
 	}
 	
 }
