e3b33aa865ec5d58522ea621223348c9fa2a6899
somename <someemail>
2017-06-08 19:57:13 +0200

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -4,8 +4,15 @@ import org.junit.Test;
 
 public class TestPlanetExplorer {
 
+	/*@Test (expected=PlanetExplorerException.class)
+	public void test_VoziloUCelijiSaPreprekom() throws PlanetExplorerException{
+		PlanetExplorer explorer = new PlanetExplorer(3,3 ,"(0,1)");
+		explorer.executeCommand("fflr");
+	}*/
 	@Test
-	public void test_executeCommand() {
-		fail("Not yet implemented");
+	public void test_SamoJednomNapred() throws PlanetExplorerException{
+		PlanetExplorer explorer = new PlanetExplorer(3,3 ,"(2,2)");
+		String result=explorer.executeCommand("f");
+		assertEquals("(0,1,N)", result);
 	}
 }
