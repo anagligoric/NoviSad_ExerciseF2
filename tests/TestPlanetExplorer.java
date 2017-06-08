a64ce4d478be5ba515882bd1c70b4ef3b57bf448
somename <someemail>
2017-06-08 20:39:09 +0200

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -40,4 +40,10 @@ public class TestPlanetExplorer {
 		String result=explorer.executeCommand("b");
 		assertEquals("(1,2,E)", result);
 	}
+	@Test 
+	public void test_ff() throws PlanetExplorerException{
+		PlanetExplorer explorer = new PlanetExplorer(3,3 ,null);
+		String result=explorer.executeCommand("b");
+		assertEquals("(0,2,N)", result);
+	}
 }
